b535c6f806d866390bf0ee4a2db14b84229c46a8
Moise-code <moisewowe@gmail.com>
2023-04-18 10:53:56 +0200

    adding the functionality to make the hours

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const clock = document.querySelector('.clock');
 const ticking = () =>{
   // now lets create the new date and assign it to the now variable
   const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+ const p = `<h1>In case you for get Time, it is</h1>
+ <p><span>${hours}h :</span> <span>${minutes}min :</span> <span>${seconds}sec</span> </p>`
+  clock.innerHTML= p;
+
 }
 // now how are we going to call this particular function 
 // we are going to use the method called setInterval.
