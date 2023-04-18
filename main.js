19cac7c9c8cffeff49c5b6b7296632cb9f27b0f0
Moise-code <moisewowe@gmail.com>
2023-04-18 13:46:53 +0200

    refactor the js file

--- main.js
+++ main.js
@@ -2,13 +2,17 @@
 const clock = document.querySelector('.clock');
 // now lets create the function and this function is going to print for us the time but on the basis of a second
 const ticking = () =>{
-  // now lets create the new date and assign it to the now variable
   const now = new Date();
+  // const TimeDifference = afterDate.getTime() - now.getTime();
+  // console.log(new Date(TimeDifference));
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
- const p = `<h1>IN CASE YOU FORGET TIME</h1>
- <p><span class="hours">${hours}h :</span> <span class="minutes">${minutes}min :</span> <span class="seconds">${seconds}sec</span> </p>`
+  
+ const p = `<h1>IN CASE YOU FORGET TIME ITS:</h1>
+ <p><span class="hours">${hours}h :</span> <span class="minutes">${minutes}min :</span> <span class="seconds">${seconds}sec </span></p>
+ `
+ 
   clock.innerHTML= p;
 
 }
