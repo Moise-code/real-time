2f3e198e9175638a991a38cdc12e3b5fd76c0072
Moise-code <moisewowe@gmail.com>
2023-04-18 10:38:30 +0200

    refactoring the codes

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 // lets just reference first
 const clock = document.querySelector('.clock');
-
 // now lets create the function and this function is going to print for us the time but on the basis of a second
+const ticking = () =>{
+  // now lets create the new date and assign it to the now variable
+  const now = new Date();
+}
+// now how are we going to call this particular function 
+// we are going to use the method called setInterval.
+
+setInterval(ticking, 1000);
