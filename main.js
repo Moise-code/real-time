5cda07585cea6e57a0233b64d56fc4ea63e9addc
Moise-code <moisewowe@gmail.com>
2023-04-20 12:08:34 +0200

    adding the codes to display the finishing and the remaining time

--- main.js
+++ main.js
@@ -5,15 +5,26 @@ const ticking = () =>{
   const now = new Date();
   // const TimeDifference = afterDate.getTime() - now.getTime();
   // console.log(new Date(TimeDifference));
+  const beforeDate = new Date('april 20 2023 12:30:00');
+  const beforeHours = beforeDate.getHours();
+  const beforeMinutes = beforeDate.getMinutes();
+  const beforeSeconds = beforeDate.getSeconds();
+
+  const dateTime = dateFns.distanceInWords(now, beforeDate);
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
   
  const p = `<h1>IN CASE YOU FORGET TIME ITS:</h1>
  <p><span class="hours">${hours}h :</span> <span class="minutes">${minutes}min :</span> <span class="seconds">${seconds}sec </span></p>
+ <h1>You are remaining with:</h1>
+ <p>${dateTime}to finish the quiz</p>
+ <h1>THE QUIZ ENDS AT:</h1>
+ <P>${beforeHours}h : ${beforeMinutes}min : ${beforeSeconds}sec</P>
  `
  
   clock.innerHTML= p;
+
 
 }
 // now how are we going to call this particular function 
